3576252c57ddd4ab97ee28e6b8c45ab091c23e3c
5h6d0w <shadow099395@gmail.com>
2026-09-15 16:33:49 -0400

    Enhance port scanner with service names and output
    Added service names for common ports and improved output formatting.

--- project1_portScanner.py
+++ project1_portScanner.py
@@ -5,20 +5,40 @@ from datetime import datetime
 
 
 
-    
+
+
 target = sys.argv[1]
 start_time = datetime.now()
-print(f"Scan Started: {start_time}\nScanning target: {target}")
+services = {
+    21: "FTP",
+    22: "SSH",
+    23: "Telnet",
+    25: "SMTP",
+    53: "DNS",
+    80: "HTTP",
+    110: "POP3",
+    143: "IMAP",
+    443: "HTTPS",
+    3389: "RDP"
+}
+        
+    
+
+print(f"[-] Scan Started: {start_time.strftime('%H:%M:%S')}\n[-] Scanning target: {target}\n")
+print("[*] Scanning ports...\n",end="", flush=True)
+
 for port in range(1, 1035):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(1)
+    sock.settimeout(0.5)
     results = sock.connect_ex((target, port))
     if results == 0:
-        print(f"Port {port} is open")
+        service_name = services.get(port, "Unknown")
+        print(f"\r[*]Port {port} ({service_name}): OPEN", flush=True)
         
         
     sock.close()
 end_time = datetime.now()
 total_time = end_time - start_time
-print(f"Scan Completed: {end_time}")
-print(f"Total Scan Time: {total_time}")
+print(f"\n[-] Scan Completed: {end_time.strftime('%H:%M:%S')}\n")
+print(f"[-] Total Scan Time: {total_time}")
+
