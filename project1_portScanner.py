a76dee0829b4efef9b8fab87e4bc2054ef86dc6b
5h6d0w <shadow099395@gmail.com>
2026-09-15 14:35:24 -0400

    Modify port scanner to use command line arguments
    Refactored port scanner to accept target as command line argument and added scan duration output.

--- project1_portScanner.py
+++ project1_portScanner.py
@@ -4,10 +4,21 @@ from datetime import datetime
 
 
 
-try: 
+
     
-    target = input("Enter the host to scan: ")
-    
-    
-except SyntaxError:
-    print("Invalid host. Please enter a valid IP address or hostname.")
+target = sys.argv[1]
+start_time = datetime.now()
+print(f"Scan Started: {start_time}\nScanning target: {target}")
+for port in range(1, 1035):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
+    results = sock.connect_ex((target, port))
+    if results == 0:
+        print(f"Port {port} is open")
+        
+        
+    sock.close()
+end_time = datetime.now()
+total_time = end_time - start_time
+print(f"Scan Completed: {end_time}")
+print(f"Total Scan Time: {total_time}")
